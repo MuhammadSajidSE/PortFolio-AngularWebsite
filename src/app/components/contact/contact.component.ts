60fc29ebd0e032a2d3caf0d781264072660a11f8
Muhammad Sajid <muhammadsajid.dh@gmail.com>
2025-09-20 19:15:02 +0500

    every thing done

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
+  sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_hfk9phh',    
+      'template_mmwdoml',   
+      e.target as HTMLFormElement,
+      'vRDtPas37k2vq3S9p'     
+    ).then(
+      (result: EmailJSResponseStatus) => {
+        console.log('SUCCESS!', result.text);
+        alert('Message sent successfully!');
+      },
+      (error) => {
+        console.log('FAILED...', error.text);
+        alert('Message failed. Please try again.');
+      }
+    );
+  }
 }
